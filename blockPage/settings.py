d6f59267035385757396f4f3f043af5faad8a067
atheerIT <atheer_manuted_it@yahoo.com>
2020-12-20 14:03:18 +0300

    backup

--- blockPage/settings.py
+++ blockPage/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'e=@ji)3r!^pvkfkqxmba6q_-g1ghxh-qbqj23%-j9zyosk+*b)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['10.1.0.101']
+ALLOWED_HOSTS = ['10.1.0.126', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -119,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
--- localPage/views.py
+++ localPage/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 import socket
+from django.http import JsonResponse, HttpResponse, HttpRequest
 
 
 # Create your views here.
-
 def index(request):
-    print(request.META['REMOTE_ADDR'])
-    print(socket.getfqdn(request.META['REMOTE_ADDR']))
-    return render(request, 'localPage/index.html',{
-        'comName':request.META['REMOTE_ADDR'],
-        'ip':socket.getfqdn(request.META['REMOTE_ADDR'])
+    return render(request, 'localPage/index.html')
+
+def info(request):
+    add= request.META['REMOTE_ADDR']
+    name = socket.getfqdn(add)
+    return JsonResponse({
+        'add': add,
+        'name': name,
     })
+
+    
